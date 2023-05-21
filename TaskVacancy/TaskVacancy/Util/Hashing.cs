ee10895681eb1614c0a9a72031bac43beba5c420
najibullo98 <najibullo.kenjaev@attolabs.de>
2023-05-21 13:27:06 +0500

    Hashing

---
+++ TaskVacancy/TaskVacancy/Util/Hashing.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace TaskVacancy.Util
+{
+    public static class Hashing
+    {
+        public static string HMACSHA1(string key, string dataToSign)
+        {
+            Byte[] secretBytes = UTF8Encoding.UTF8.GetBytes(key);
+            HMACSHA1 hmac = new HMACSHA1(secretBytes);
+
+            Byte[] dataBytes = UTF8Encoding.UTF8.GetBytes(dataToSign);
+            Byte[] calcHash = hmac.ComputeHash(dataBytes);
+            String calcHashString = Convert.ToBase64String(calcHash);
+            return calcHashString;
+        }
+    }
+}
